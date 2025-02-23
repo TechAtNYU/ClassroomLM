15c0173c7aec34c83ba6e16ec0e0d31edc3035cd
safipatel <safipatel1@gmail.com>
2025-02-23 20:32:31 +0000

    husky test

--- app/api/health/route.test.ts
+++ app/api/health/route.test.ts
@@ -1,7 +1,6 @@
-import { expect, test } from 'vitest'
-import { GET } from './route'
+import { expect, test } from "vitest";
+import { GET } from "./route";
 
-test('Get 200 health check response', () => {
-    expect(GET()).resolves.toHaveProperty("status",200)
-})
-
+test("Get 200 health check response", () => {
+  expect(GET()).resolves.toHaveProperty("status", 200);
+});

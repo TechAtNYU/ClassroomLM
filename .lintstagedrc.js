5e2183734212f1d7c25fde71e4dfa9f8c89936b1
chenxin-yan <71162231+chenxin-yan@users.noreply.github.com>
2025-02-24 11:12:52 -0500

    remove `pnpm format` in pre-commit hook (#6)
    Co-authored-by: safipatel <safipatel1@gmail.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,6 +5,13 @@ const buildEslintCommand = (filenames) =>
     .map((f) => relative(process.cwd(), f))
     .join(" --file ")}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --ignore-unknown --check ${filenames
+    .map((f) => relative(process.cwd(), f))
+    .join(" ")}`;
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+  "*.{json,yml,yaml,md}": [buildPrettierCommand],
 };

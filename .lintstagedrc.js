561a8f8da337a880f3ee39af717bdf7b6ff914e4
safipatel <safipatel1@gmail.com>
2025-02-24 16:08:50 +0000

    add message for vscode popup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,11 +6,12 @@ const buildEslintCommand = (filenames) =>
     .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --check ${filenames
+  `prettier --ignore-unknown --check ${filenames
     .map((f) => relative(process.cwd(), f))
     .join(" ")}`;
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+  "*.{json,yml,yaml,md}": [buildPrettierCommand],
 };

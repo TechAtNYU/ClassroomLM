82066c80d0441c70096af9892940e3c4b4adecc6
chenxin-yan <cxyan04@gmail.com>
2025-02-24 21:00:52 -0500

    add OAuth page, auth callback endpoint, and error page

---
+++ app/login/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export async function signInWithGoogle() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    console.error("OAuth error:", error);
+    return redirect(`/error`);
+  }
+
+  if (data.url) {
+    redirect(data.url);
+  }
+}

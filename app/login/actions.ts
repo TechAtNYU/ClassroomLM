e6b88a71f498b643b931e67c78632b5e167aa4a9
chenxin-yan <cxyan04@gmail.com>
2025-02-24 23:20:11 -0500

    fix user not prompt google auth

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -10,6 +10,9 @@ export async function signInWithGoogle() {
     provider: "google",
     options: {
       redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+      queryParams: {
+        prompt: "select_account",
+      },
     },
   });
 

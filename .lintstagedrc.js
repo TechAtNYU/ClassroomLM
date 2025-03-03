1b5b2a043c6ffb5cff7ce86ea8f935b1a608f329
Xiaomin Liu <xl4624@nyu.edu>
2025-03-02 21:41:50 -0500

    Update pre-commit hook to auto-fix issues

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,12 @@
 import { relative } from "path";
 
 const buildEslintCommand = (filenames) =>
-  `next lint --file ${filenames
+  `next lint --fix --file ${filenames
     .map((f) => relative(process.cwd(), f))
     .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --ignore-unknown --check ${filenames
+  `prettier --ignore-unknown --write ${filenames
     .map((f) => relative(process.cwd(), f))
     .join(" ")}`;
 

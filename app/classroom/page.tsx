66d860111551e2cfd83922cab789db7553a42773
safipatel <safipatel1@gmail.com>
2025-03-09 04:55:43 +0000

    capitalized button

--- app/classroom/page.tsx
+++ app/classroom/page.tsx
@@ -30,7 +30,7 @@ export default async function ClassroomPage() {
                 className="mb-2 me-2 rounded-lg border border-red-700 px-5 py-2.5 text-center text-sm font-medium text-red-700 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900"
                 onClick={deleteClassroomWithId}
               >
-                Delete classroom
+                Delete Classroom
               </button>
               <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
             </div>

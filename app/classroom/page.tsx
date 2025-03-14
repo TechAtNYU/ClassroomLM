19f962a6303a8dfabc065745009675984420a2a7
Emily <emily.silkin@gmail.com>
2025-03-14 17:40:59 -0400

    Added input box and worked on styling

--- app/classroom/page.tsx
+++ app/classroom/page.tsx
@@ -97,7 +97,7 @@ export default async function ClassroomPage() {
         <Link href="newClassroom/">
           <button
             type="button"
-            className="mb-2 me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900"
+            className="dark:focus:green-red-900 mb-2 me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white"
           >
             Create a Classroom
           </button>
--- app/newClassroom/page.tsx
+++ app/newClassroom/page.tsx
@@ -1,10 +1,44 @@
-export default async function NewClassroomPage() {
+"use client";
+import { useState } from "react";
+
+export default function NewClassroomPage() {
+  const [className, setClassName] = useState("");
   return (
     <>
-      <div>
-        <h1>Create a new classroomw</h1>
-        <button>Add Classroom</button>
-        <button>Back to My Classrooms</button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1 style={{ paddingTop: "20px" }}>Create a new classroom!</h1>
+        <div
+          style={{ flexDirection: "row", display: "flex", paddingTop: "20px" }}
+        >
+          <h2 style={{ paddingRight: "10px" }}>Name:</h2>
+          <input
+            type="email"
+            placeholder="Enter classroom name"
+            value={className}
+            onChange={(e) => setClassName(e.target.value)}
+            style={{
+              color: "black",
+              outline: "2px solid black",
+              outlineColor: "black",
+            }}
+          />
+        </div>
+
+        <div style={{ paddingTop: "20px" }}>
+          <button className="dark:focus:green-red-900 mb-2 me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white">
+            Add Classroom
+          </button>
+          <button className="mb-2 me-2 rounded-lg border border-red-700 px-5 py-2.5 text-center text-sm font-medium text-red-700 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900">
+            Back to My Classrooms
+          </button>
+        </div>
       </div>
     </>
   );

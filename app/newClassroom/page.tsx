e1a746f753d98d9788f01f820ae12d429ee7d887
Emily <emily.silkin@gmail.com>
2025-03-14 15:07:50 -0400

    Added basic frontend

---
+++ app/newClassroom/page.tsx
@@ -0,0 +1,11 @@
+export default async function NewClassroomPage() {
+  return (
+    <>
+      <div>
+        <h1>Create a new classroomw</h1>
+        <button>Add Classroom</button>
+        <button>Back to My Classrooms</button>
+      </div>
+    </>
+  );
+}

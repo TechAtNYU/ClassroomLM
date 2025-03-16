4887d79b84551d1ab31b5216206c631c7e27b9ab
safipatel <safi.patel@nyu.edu>
2025-03-16 16:20:23 +0000

    added database types

---
+++ utils/supabase/database.types.ts
@@ -0,0 +1,256 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export type Database = {
+  public: {
+    Tables: {
+      Allowed_Domains: {
+        Row: {
+          domain: string;
+          id: number;
+        };
+        Insert: {
+          domain: string;
+          id?: number;
+        };
+        Update: {
+          domain?: string;
+          id?: number;
+        };
+        Relationships: [];
+      };
+      Chat_Rooms: {
+        Row: {
+          classroom_id: number;
+          created_at: string;
+          id: number;
+        };
+        Insert: {
+          classroom_id: number;
+          created_at?: string;
+          id?: number;
+        };
+        Update: {
+          classroom_id?: number;
+          created_at?: string;
+          id?: number;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "Chat_Rooms_classroom_id_fkey";
+            columns: ["classroom_id"];
+            isOneToOne: false;
+            referencedRelation: "Classroom";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      Classroom: {
+        Row: {
+          admin_user_id: string | null;
+          chat_assistant_id: string | null;
+          created_at: string;
+          id: number;
+          metadata: Json | null;
+          name: string | null;
+          ragflow_dataset_id: string | null;
+        };
+        Insert: {
+          admin_user_id?: string | null;
+          chat_assistant_id?: string | null;
+          created_at?: string;
+          id?: number;
+          metadata?: Json | null;
+          name?: string | null;
+          ragflow_dataset_id?: string | null;
+        };
+        Update: {
+          admin_user_id?: string | null;
+          chat_assistant_id?: string | null;
+          created_at?: string;
+          id?: number;
+          metadata?: Json | null;
+          name?: string | null;
+          ragflow_dataset_id?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "Classroom_admin_user_id_fkey";
+            columns: ["admin_user_id"];
+            isOneToOne: false;
+            referencedRelation: "Users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      Classroom_Members: {
+        Row: {
+          classroom_id: number | null;
+          created_at: string;
+          id: number;
+          ragflow_session_id: string | null;
+          user_id: string | null;
+        };
+        Insert: {
+          classroom_id?: number | null;
+          created_at?: string;
+          id?: number;
+          ragflow_session_id?: string | null;
+          user_id?: string | null;
+        };
+        Update: {
+          classroom_id?: number | null;
+          created_at?: string;
+          id?: number;
+          ragflow_session_id?: string | null;
+          user_id?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "Classroom_Members_classroom_id_fkey";
+            columns: ["classroom_id"];
+            isOneToOne: false;
+            referencedRelation: "Classroom";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "Classroom_Members_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "Users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      Users: {
+        Row: {
+          email: string;
+          id: string;
+        };
+        Insert: {
+          email: string;
+          id?: string;
+        };
+        Update: {
+          email?: string;
+          id?: string;
+        };
+        Relationships: [];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
+
+type PublicSchema = Database[Extract<keyof Database, "public">];
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;

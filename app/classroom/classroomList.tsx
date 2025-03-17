344cb27135ad22a494a03e20ccdac82ba1157f7f
emsilkina <118944864+emsilkina@users.noreply.github.com>
2025-03-17 13:58:39 -0400

    Added a button from classroom page to appropriate upload material page (#27)

--- app/classroom/classroomList.tsx
+++ app/classroom/classroomList.tsx
@@ -78,6 +78,18 @@ export default function ClassroomList({
           >
             {isAdmin ? "Delete Classroom" : "Leave Classroom"}
           </button>
+
+          {isAdmin && (
+            <Link href={`../upload/${classroom.id}`} passHref>
+              <button
+                type="button"
+                className="me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
+              >
+                Upload Materials
+              </button>
+            </Link>
+          )}
+
           <hr className="my-5 h-px border-0 bg-gray-800 dark:bg-white" />
         </div>
       );

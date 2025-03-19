41cbeda1ce6218c07c2d633dae480c158d1ceeca
Xiaomin Liu <xl4624@nyu.edu>
2025-03-19 14:45:34 -0400

    Chat assistant (#20)
    Co-authored-by: Pranit <pg2647@nyu.edu>
    Co-authored-by: Shubham Parab <71990977+Skparab1@users.noreply.github.com>
    Co-authored-by: Benjamin <ben@art.com>
    Co-authored-by: skparab1 <skparab1@gmail.com>
    Co-authored-by: safipatel <safi.patel@nyu.edu>

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,6 +1,5 @@
 export async function GET() {
   return Response.json({
     status: "OK",
-    secret_env_test: process.env.SECRET_TEST,
   });
 }
--- app/chat/[classroomId]/page.tsx
+++ app/chat/[classroomId]/page.tsx
@@ -1,4 +1,13 @@
-import { getCurrentUserId, getRagflowDatasetId } from "./actions";
+import {
+  getCurrentUserId,
+  getRagflowDatasetId,
+  getOrCreateAssistant,
+  getOrCreateSession,
+  getDisplayInfo,
+  retrieveMessageHistory,
+} from "./actions";
+
+import MessageBox from "./MessageBox";
 
 export default async function ChatPage({
   params,
@@ -7,19 +16,59 @@ export default async function ChatPage({
 }) {
   const userId = await getCurrentUserId();
   const { classroomId } = await params;
-  const datasetId = getRagflowDatasetId(Number(classroomId));
+  const classroomIdNum = Number(classroomId);
+  const datasetId = await getRagflowDatasetId(classroomIdNum);
 
+  if (!datasetId) {
+    return <h1>No dataset found!</h1>;
+  }
+
+  const chatAssistantId = await getOrCreateAssistant(
+    Number(classroomId),
+    datasetId,
+    userId
+  );
+
+  const chatSessionId = await getOrCreateSession(
+    userId,
+    chatAssistantId,
+    classroomIdNum
+  );
+
+  const messageHistory = await retrieveMessageHistory(
+    chatAssistantId,
+    userId,
+    chatSessionId
+  );
+
+  const displayInfo = await getDisplayInfo(classroomIdNum, userId);
+
+  // console.log("chatAssistant", chatAssistant);
   return (
     <div className="p-4 text-gray-800 dark:text-white">
+      {/* <p>
+        <strong>Classroom ID: </strong>
+        {classroomId}
+        <br></br>
+        <strong>User ID: </strong>
+        {userId} <br></br>
+        <strong>Ragflow Dataset ID:</strong> {datasetId} <br></br>
+        <strong>Chat Assistant ID:</strong> {chatAssistantId} <br></br>
+        <strong>Chat Session ID:</strong> {chatSessionId}
+      </p> */}
       <p>
-        <strong>Classroom ID:</strong> {classroomId}
-      </p>
-      <p>
-        <strong>User ID:</strong> {userId}
-      </p>
-      <p>
-        <strong>Ragflow Dataset ID:</strong> {datasetId}
-      </p>
+        <strong>Welcome to: </strong>
+        {displayInfo[0]}, <strong>{displayInfo[1]}</strong> <br />
+        <strong>Ragflow Dataset ID:</strong> {datasetId} <br />
+        <strong>Chat Assistant ID:</strong> {chatAssistantId} <br />
+        <strong>Chat Session ID:</strong> {chatSessionId}
+      </p>{" "}
+      <MessageBox
+        assistantId={chatAssistantId}
+        userId={userId}
+        chatSessionId={chatSessionId}
+        messageHistory={messageHistory}
+      ></MessageBox>
     </div>
   );
 }

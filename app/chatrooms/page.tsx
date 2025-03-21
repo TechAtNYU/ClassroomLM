4c2ab2e4a93ecf774a3ff47295d87cd6b79203ae
chenxin-yan <cxyan04@gmail.com>
2025-03-21 16:04:38 -0400

    implement page `/chatrooms`

--- app/chatrooms/page.tsx
+++ app/chatrooms/page.tsx
@@ -1,5 +1,57 @@
-// TODO: display all chatrooms that current user joined
+import { createClient } from "@/utils/supabase/server";
 
-// import { getCurrentUserId } from "../chat/[classroomId]/actions";
-//
-// const currentUser = await getCurrentUserId();
+const ChatroomsPage = async () => {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    throw new Error("No authenticated user found");
+  }
+  const currentUser = user.id;
+  const classroomMembers = await supabase
+    .from("Classroom_Members")
+    .select("id")
+    .eq("user_id", currentUser);
+
+  const memberIds = classroomMembers.data?.map((element) => element.id) || [];
+
+  const chatroomIds = [];
+
+  for (const memberId of memberIds) {
+    const { data, error } = await supabase
+      .from("Chatroom_Members")
+      .select("chatroom_id")
+      .eq("member_id", memberId);
+
+    if (data && !error) {
+      for (const element of data) {
+        chatroomIds.push(element.chatroom_id);
+      }
+    }
+  }
+  console.log(chatroomIds);
+
+  const chatrooms = [];
+
+  for (const chatroomId of chatroomIds) {
+    const { data, error } = await supabase
+      .from("Chatrooms")
+      .select("*")
+      .eq("id", chatroomId)
+      .single();
+
+    if (data && !error) {
+      chatrooms.push(data);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Your Chatrooms</h1>
+      <pre>{JSON.stringify(chatrooms, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default ChatroomsPage;

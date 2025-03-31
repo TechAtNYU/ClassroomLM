0981853868eb5add891f126d99ae68e2fde09c57
Emily <emily.silkin@gmail.com>
2025-03-31 14:08:09 -0400

    Added back button

--- app/classroomManagement/[classroomId]/page.tsx
+++ app/classroomManagement/[classroomId]/page.tsx
@@ -4,6 +4,7 @@
 // import { getCurrentUserId, retrieveClassroomData } from "../../classroom/actions";
 
 import ClassroomManagementButtons from "./buttons";
+import Link from "next/link";
 
 export default async function ClassroomManagementPage({
   params,
@@ -19,6 +20,14 @@ export default async function ClassroomManagementPage({
     <div>
       <h1>Hello this is classroom {classroomId}</h1>
       <ClassroomManagementButtons classroomId={classroomIdNumber} />
+      <Link href={`../classroom`} passHref>
+        <button
+          type="button"
+          className="me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
+        >
+          Classroom Page
+        </button>
+      </Link>
     </div>
   );
 }

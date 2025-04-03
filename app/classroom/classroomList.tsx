e0e73d384a7c0564849bed067f57906dc732603e
Emily <emily.silkin@gmail.com>
2025-04-03 15:34:33 -0400

    addedMembersListManagementPage

--- app/classroom/classroomList.tsx
+++ app/classroom/classroomList.tsx
@@ -28,7 +28,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import { FaEdit, FaUsers } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 import { AiOutlineUserDelete } from "react-icons/ai";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
 
@@ -112,7 +112,7 @@ export default function ClassroomList() {
                     </CardDescription>
                   </CardHeader>
                   <CardContent>
-                    <TooltipProvider>
+                    {/* <TooltipProvider>
                       <Tooltip delayDuration={300}>
                         <TooltipTrigger>
                           {!isAdmin && (
@@ -136,18 +136,18 @@ export default function ClassroomList() {
                         </TooltipTrigger>
                         <TooltipContent>Leave Classroom</TooltipContent>
                       </Tooltip>
-                    </TooltipProvider>
+                    </TooltipProvider> */}
 
                     <TooltipProvider>
                       <Tooltip delayDuration={300}>
                         <TooltipTrigger>
                           <Link href={`../chat/${classroom.id}`} passHref>
-                            <button
+                            {/* <button
                               type="button"
                               className="me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
-                            >
-                              <IoChatboxEllipsesOutline />
-                            </button>
+                            > */}
+                            <IoChatboxEllipsesOutline />
+                            {/* </button> */}
                           </Link>
                         </TooltipTrigger>
                         <TooltipContent>Chat!</TooltipContent>
@@ -160,14 +160,9 @@ export default function ClassroomList() {
                           {isAdmin && (
                             <Link
                               href={`/classroom/${classroom.id}/manage`}
-                              passHref
+                              // className="me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
                             >
-                              <button
-                                type="button"
-                                className="me-2 rounded-lg border border-green-700 px-5 py-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
-                              >
-                                <FaEdit />
-                              </button>
+                              <FaEdit />
                             </Link>
                           )}
                         </TooltipTrigger>

fc47f78ca441ac8f480dca31bcc7c3ea38d6b56b
Safi Patel <safipatel1@gmail.com>
2025-04-04 19:51:11 -0400

    fixed logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,6 @@ export default async function RootLayout({
                       {children}
                       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
                     </div>
-                    
                   </main>
                   <Toaster />
                 </SidebarInset>

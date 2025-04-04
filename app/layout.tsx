e27a5354c228a79ce35dc7be289d450232e44f71
Safi Patel <safipatel1@gmail.com>
2025-04-04 05:57:16 -0400

    made inset formatting proper

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import { AppSidebar } from "@/components/ui/sidebar/app-sidebar";
 import UserContextProvider from "./lib/userContext/userContext";
 import { getUserAndClassroomData } from "./lib/userContext/contextFetcher";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,8 +59,20 @@ export default async function RootLayout({
                 <AppSidebar />
                 <SidebarInset>
                   <main>
-                    <SidebarTrigger />
-                    {children}
+                    <header className="flex h-16 shrink-0 items-center gap-2">
+                      <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                          orientation="vertical"
+                          className="mr-2 h-4"
+                        />
+                      </div>
+                    </header>
+                    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                      {children}
+                      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                    </div>
+                    
                   </main>
                   <Toaster />
                 </SidebarInset>

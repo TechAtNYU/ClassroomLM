c338738abf21c210b94e4abb6e4ebc2f1c470b1a
chenxin-yan <71162231+chenxin-yan@users.noreply.github.com>
2025-04-09 08:32:14 -0400

    fix link to personal chat in classroom cards (#56)

--- app/classroom/classroomList.tsx
+++ app/classroom/classroomList.tsx
@@ -317,7 +317,7 @@ function ClassroomListWithContext({
                 asChild
                 // className="me-2 rounded-lg border px-5 py-2.5 text-center text-sm font-medium hover:bg-green-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-600 dark:hover:text-white dark:focus:ring-green-900"
               >
-                <Link href={`../chat/${classroom.id}`} passHref>
+                <Link href={`/classroom/${classroom.id}/chat`} passHref>
                   <MessageSquareMore className="scale-[200%]" />{" "}
                 </Link>
               </Button>
